55cae02b0b95042a1bfaff31956a545cd81e30a8
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-12-21 16:07:41 +0900

    feat: Nest.js 연습 - 노마드 코더 22-12-21 16:07

---
+++ src/movies/dto/update-movie.dto.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateMovieDto } from './create-movie.dto';
+
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
